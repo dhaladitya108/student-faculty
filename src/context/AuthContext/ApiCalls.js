7704c83cb7e9bd422d79f43b013e4579c8a5bb35
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2022-06-04 18:16:58 +0530

    removed unecessary imports

--- src/context/AuthContext/ApiCalls.js
+++ src/context/AuthContext/ApiCalls.js
@@ -1,49 +1,49 @@
 import axios from "axios";
-import { loginFailure, loginStart, loginSuccess } from "./AuthActions";
-// import { useNavigate } from "react-router-dom";
+import { loginFailure, loginSuccess } from "./AuthActions";
 export const login = async (user, dispatch) => {
-
   try {
     const res = await axios.post("https://iwtserver.herokuapp.com/login", user);
 
     dispatch(loginSuccess(res.data));
-    // navigate("/");
   } catch (error) {
     dispatch(loginFailure());
   }
 };
 
 export const register = async (user, dispatch) => {
-
   try {
-    const res = await axios.post("https://iwtserver.herokuapp.com/register", user);
+    const res = await axios.post(
+      "https://iwtserver.herokuapp.com/register",
+      user
+    );
 
     dispatch(loginSuccess(res.data));
-    // navigate("/");
   } catch (error) {
     dispatch(loginFailure());
   }
 };
 
 export const facultylogin = async (user, dispatch) => {
-
   try {
-    const res = await axios.post("https://iwtserver.herokuapp.com/facultylogin", user);
+    const res = await axios.post(
+      "https://iwtserver.herokuapp.com/facultylogin",
+      user
+    );
 
     dispatch(loginSuccess(res.data));
-    // navigate("/");
   } catch (error) {
     dispatch(loginFailure());
   }
 };
 
 export const facultyregister = async (user, dispatch) => {
-
   try {
-    const res = await axios.post("https://iwtserver.herokuapp.com/facultyregister", user);
+    const res = await axios.post(
+      "https://iwtserver.herokuapp.com/facultyregister",
+      user
+    );
 
     dispatch(loginSuccess(res.data));
-    // navigate("/");
   } catch (error) {
     dispatch(loginFailure());
   }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import React, { useContext } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext/AuthContext";
-// import {AuthContext} from '../src/context/AuthContext/'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// const { user } = useContext(AuthContext);
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
